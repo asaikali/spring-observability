31c288f68ad86702e119ab18258cad70abe8a188
Adib Saikali <asaikali@gmail.com>
2025-05-07 08:14:48 -0400

    Polish

--- config/2-traces-to-tempo/src/main/java/com/example/quotes/QuoteController.java
+++ config/2-traces-to-tempo/src/main/java/com/example/quotes/QuoteController.java
@@ -2,6 +2,8 @@ package com.example.quotes;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +15,22 @@ public class QuoteController {
   private final Logger log = LoggerFactory.getLogger(QuoteController.class);
   private final QuoteRepository quoteRepository;
   private final Counter generatedQuotes;
+  private final Tracer tracer;
 
-  public QuoteController(QuoteRepository quoteRepository, MeterRegistry meterRegistry) {
+  public QuoteController(QuoteRepository quoteRepository, MeterRegistry meterRegistry, Tracer tracer) {
     this.quoteRepository = quoteRepository;
     this.generatedQuotes = meterRegistry.counter("quotes.generated");
+    this.tracer = tracer;
   }
 
   @GetMapping("/random-quote")
   public Quote randomQuote() {
     Quote result = quoteRepository.findRandomQuote();
     this.generatedQuotes.increment();
+    Span currentSpan = tracer.currentSpan();
+    if( currentSpan != null) {
+      currentSpan.tag("quote", result.getQuote());
+    }
     log.info("Random quote generated: {} by {}", result.getQuote(), result.getAuthor());
     return result;
   }
