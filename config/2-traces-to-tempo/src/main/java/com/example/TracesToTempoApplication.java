768f6fa85a6f68183261b539821b212e3593ac06
Adib Saikali <asaikali@gmail.com>
2025-05-07 15:39:50 -0400

    Polish

--- config/2-traces-to-tempo/src/main/java/com/example/TracesToTempoApplication.java
+++ config/2-traces-to-tempo/src/main/java/com/example/TracesToTempoApplication.java
@@ -1,10 +1,30 @@
 package com.example;
 
+import brave.handler.MutableSpan;
+import brave.handler.SpanHandler;
+import brave.propagation.TraceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TracesToTempoApplication {
+
+
+  private static final Logger log = LoggerFactory.getLogger(TracesToTempoApplication.class);
+
+  @Bean
+  SpanHandler spanHandler() {
+    return new SpanHandler() {
+      @Override
+      public boolean end(TraceContext context, MutableSpan span, Cause cause) {
+        log.info("Sending span to Tempo: {}", span.tags());
+        return super.end(context, span, cause);
+      }
+    };
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TracesToTempoApplication.class, args);
--- config/5-metrics-to-otel/src/main/java/com/example/quotes/QuoteController.java
+++ config/5-metrics-to-otel/src/main/java/com/example/quotes/QuoteController.java
@@ -2,8 +2,6 @@ package com.example.quotes;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.tracing.Span;
-import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,26 +13,16 @@ public class QuoteController {
   private final Logger log = LoggerFactory.getLogger(QuoteController.class);
   private final QuoteRepository quoteRepository;
   private final Counter generatedQuotes;
-  private final Tracer tracer;
 
-  public QuoteController(
-      QuoteRepository quoteRepository, MeterRegistry meterRegistry, Tracer tracer) {
+  public QuoteController(QuoteRepository quoteRepository, MeterRegistry meterRegistry) {
     this.quoteRepository = quoteRepository;
     this.generatedQuotes = meterRegistry.counter("quotes.generated");
-    this.tracer = tracer;
   }
 
   @GetMapping("/random-quote")
   public Quote randomQuote() {
     Quote result = quoteRepository.findRandomQuote();
     this.generatedQuotes.increment();
-    Span currentSpan = tracer.currentSpan();
-    if (currentSpan != null) {
-      currentSpan.tag("quote", result.getQuote());
-//      currentSpan.tag("test", "test");
-//      currentSpan.event("quoted");
-      log.info("quoted added to current span: {}", result.getQuote());
-    }
     log.info("Random quote generated: {} by {}", result.getQuote(), result.getAuthor());
     return result;
   }
