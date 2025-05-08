83178ea39bdc0915d556192d9984ba87fe4c7c90
Adib Saikali <asaikali@gmail.com>
2025-05-07 23:38:18 -0400

    add all-to-otel example

---
+++ config/7-all-to-otel/src/main/java/com/example/AllToOtelApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllToOtelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AllToOtelApplication.class, args);
+  }
+}
